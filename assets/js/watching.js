64671a030732668a1bd13d26fd865786adfb3326
yuji masujima <masjima2712@gmail.com>
2020-11-15 17:45:17 +0900

    Update happouNiramiNeko.html

--- assets/js/watching.js
+++ assets/js/watching.js
@@ -1,12 +1,15 @@
 $(function() {
   var objWidth;
   var translatePropaty;
+  var shadowPropaty;
   var changeText = $('.offset').on('mousemove', function(e) {
-    objWidth = $(this).width() / 100;
-    $('.offset-x').text(e.offsetX);
-    $('.offset-y').text(e.offsetY);
-    translatePropaty = `translate(${e.offsetX / objWidth - 50}%,${e.offsetY / objWidth - 50}%)`;
-    $("div.eye").css("transform", translatePropaty);
+      objWidth = $(this).width() / 100;
+      $('.offset-x').text((e.offsetX / $(this).width() * 100 - 50) / 10);
+      $('.offset-y').text((e.offsetY / $(this).height() * 100 - 50) / 10);
+      translatePropaty = `translate(${e.offsetX / objWidth - 50}%,${e.offsetY / objWidth - 50}%)`;
+      $("div.eye").css("transform", translatePropaty);
+      shadowPropaty = `${(e.offsetX / $(this).width() * 100 - 50) / 10}px ${(e.offsetY / $(this).height() * 100 - 50) / 10}px 30px 0px gray inset`;
+      $("div.face").css("box-shadow", shadowPropaty);
   });
   $(".offset").hover(
     function () {
@@ -16,9 +19,9 @@ $(function() {
       $('.offset-x').text("0");
       $('.offset-y').text("0");
       $("div.eye").css("transform", "translate(0)");
+      $("div.face").css("box-shadow", "0px 0px 30px 0px gray inset");
     }
   );
-	
 }); 
 
 
